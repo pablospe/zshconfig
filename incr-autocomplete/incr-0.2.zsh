98fbfb19aac1262ee8491a0840ea309438f61e44
Pablo Speciale <pablo.speciale@gmail.com>
2014-03-26 22:59:15 -0300

    Improved startup time considerably. Note: compinit should be call only once. http://blog.patshead.com/2011/04/improve-your-oh-my-zsh-startup-time-maybe.html

--- incr-autocomplete/incr-0.2.zsh
+++ incr-autocomplete/incr-0.2.zsh
@@ -1,14 +1,11 @@
 # Incremental completion for zsh
 # by y.fujii <y-fujii at mimosa-pudica.net>, public domain
 
-
-autoload -U compinit
 zle -N self-insert self-insert-incr
 # zle -N vi-cmd-mode-incr
 # zle -N vi-backward-delete-char-incr
 zle -N backward-delete-char-incr
 zle -N expand-or-complete-prefix-incr
-compinit
 
 # bindkey -M viins '^[' vi-cmd-mode-incr
 # bindkey -M viins '^h' vi-backward-delete-char-incr
