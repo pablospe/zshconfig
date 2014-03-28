20341b1cab3bd5216ab010dda48ea3a2e705c4c1
Pablo Speciale <pablo.speciale@gmail.com>
2014-03-28 15:58:39 -0300

    Color for less (manpages)

--- export.sh
+++ export.sh
@@ -5,3 +5,11 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'
 
+# Color for less (manpages)
+export LESS_TERMCAP_md=$'\e[01;34m';
+export LESS_TERMCAP_us=$'\e[01;31m';
+export LESS_TERMCAP_so=$'\e[01;32m';
+export LESS_TERMCAP_ue=$'\e[00;00m';
+export LESS_TERMCAP_se=$'\e[00;00m';
+# export LESS_TERMCAP_me=$'\e[00;00m';
+# export LESS_TERMCAP_mb=$'\e[00;00m';
