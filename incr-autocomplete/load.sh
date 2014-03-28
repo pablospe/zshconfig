aa54246e8a121149bc4b6cdfbd2ea0ce093ba07a
Pablo Speciale <pablo.speciale@gmail.com>
2014-03-28 00:58:27 -0300

    Incremental autocomplete with highlighting (it is not perfect yet)

--- incr-autocomplete/load.sh
+++ incr-autocomplete/load.sh
@@ -8,7 +8,6 @@ bindkey -M menuselect '^M' .accept-line
 zstyle ':completion:*' completer _expand _complete #_approximate
 # zstyle ':completion:*:*:*:default' menu yes scroll=0 select=2 interactive 
 zstyle ':completion:*:*:default' force-list always
-# 
-# autoload -U colors
-# zstyle -e ':completion:*' list-colors 'reply=( "=(#b)(*$PREFIX)(?)*=00=$color[underline]" )'
 
+autoload -U colors
+zstyle -e ':completion:*' list-colors 'reply=( "=(#b)(*$PREFIX)(?)*=00=$color[green]" )'
