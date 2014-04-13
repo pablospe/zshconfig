935b1bbb812923e00a1b5264d64099ef825a64d7
Pablo Speciale <pablo.speciale@gmail.com>
2014-04-12 22:19:30 -0300

    Added colorgcc

--- export.sh
+++ export.sh
@@ -5,6 +5,18 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'
 
+##
+## colorgcc
+##
+# sudo apt-get install colorgcc
+# sudo mkdir -p /usr/lib/colorgcc/
+# sudo ln -s /usr/bin/colorgcc gcc
+# sudo ln -s /usr/bin/colorgcc g++
+# sudo ln -s /usr/bin/colorgcc cc
+# sudo ln -s /usr/bin/colorgcc c+
+##
+export PATH=/usr/lib/colorgcc:$PATH
+
 # Color for less (manpages)
 export LESS_TERMCAP_md=$'\e[01;34m';
 export LESS_TERMCAP_us=$'\e[01;31m';
