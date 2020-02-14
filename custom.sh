b062c3a1a8692586535c06b4d828d0dbd6201be6
Pablo Speciale <pablo.speciale@gmail.com>
2020-02-13 22:32:57 -0300

    Give a try to 'zhh-interactive-cd.plugin.zsh' and 'forgit.plugin.zsh.sh' (see 'glo' and 'ga'). From: https://github.com/changyuheng/zsh-interactive-cd

--- custom.sh
+++ custom.sh
@@ -79,3 +79,10 @@ fi
 bindkey '^X^A' fasd-complete    # C-x C-a to do fasd-complete (fils and directories)
 bindkey '^X^F' fasd-complete-f  # C-x C-f to do fasd-complete-f (only files)
 bindkey '^X^D' fasd-complete-d  # C-x C-d to do fasd-complete-d (only directories)
+
+
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source ${ZSH_PWD}/fzf/zsh-interactive-cd.plugin.zsh
+source ${ZSH_PWD}/fzf/forgit.plugin.zsh
+
