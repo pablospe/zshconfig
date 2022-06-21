31e07f6e51129b3ab02f2b9dec99e2d4be182a66
Pablo Speciale <Pablo.Speciale@microsoft.com>
2022-06-21 22:48:48 +0200

    Update update.sh.

--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 # Pull the latest changes and update submodules
-git pull && git submodule update --init --recursive
+git pull && git submodule update --init --recursive --remote
+
+ROOT=$(pwd)
+
+# Update powerlevel10k.
+cd "${ROOT}/oh-my-zsh/custom/themes/powerlevel10k" && git pull
+
+# Update zsh-autosuggestions.
+cd "${ROOT}/oh-my-zsh/custom/plugins/zsh-autosuggestions" && git pull
+
