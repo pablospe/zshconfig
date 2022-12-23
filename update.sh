16851f5d83961445967beddbd7fc636c6560c282
Pablo Speciale <Pablo.Speciale@microsoft.com>
2022-12-23 23:41:33 +0100

    Keep track on master, see:   https://stackoverflow.com/a/18799234/4562968

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-# Pull the latest changes and update submodules
+# Prefix data and color for every command.
+PS4='\033[1;31m$(date +%H:%M:%S)\033[0m '
+set -exo pipefail
+
+# Pull the latest changes and update submodules.
 git pull && git submodule update --init --recursive --remote
+
+# Keep track on master.
+# https://stackoverflow.com/a/18799234/4562968
+git submodule foreach -q --recursive 'echo $name; git switch $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master) && git pull'
