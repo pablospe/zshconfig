9e064fbc71fe7308f486847d993ce5cff529b9fa
Pablo Speciale <Pablo.Speciale@microsoft.com>
2024-02-26 23:59:13 +0100

    parallel version, need more testing.

--- update.sh
+++ update.sh
@@ -9,4 +9,6 @@ git pull && git submodule update --init --recursive --remote
 
 # Keep track on master.
 # https://stackoverflow.com/a/18799234/4562968
-git submodule foreach -q --recursive 'echo $name; git switch $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master) && git pull'
+# git submodule foreach -q --recursive 'echo $name; git switch $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master) && git pull'
+git submodule foreach -q --recursive 'echo $path' | xargs -P10 -I {} bash -c 'echo "Updating $1 ..." && cd "$1" && branch=$(git config -f "$(git rev-parse --show-toplevel)/.gitmodules" submodule."$1".branch || echo master) && git switch "$branch" && git pull' bash {}
+
