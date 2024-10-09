4cc410c1d4e17a1b01c70783c5cdc686aa2d525a
Pablo Speciale <Pablo.Speciale@microsoft.com>
2024-10-09 16:12:05 +0200

    fix comment

--- bindkey.sh
+++ bindkey.sh
@@ -94,7 +94,8 @@ bindkey "\e\e" kill-whole-line
 ## dirhistory
 ##
 ## My implementation of the dirhistory plugin, since there are problems
-## 'last-working-dir' plugin. It enables cycling through the directory## stack using Alt+Left/Right
+## 'last-working-dir' plugin. It enables cycling through the directory
+## stack using Alt+Left/Right
 ##
 
 # Alt+Left
