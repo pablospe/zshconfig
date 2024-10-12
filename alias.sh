09ebf1f8941871f1284d6bbd104a12bbb4123f7e
Pablo Speciale <pablospe@users.noreply.github.com>
2024-10-13 00:10:20 +0200

    clean

--- alias.sh
+++ alias.sh
@@ -54,18 +54,12 @@ if hash dfc 2>/dev/null; then
   alias df="dfc -f -T -q "name" -t ext,fuse"
 fi
 
-# Colorized cat (sudo apt-get install python-pygments)
-alias ccat='pygmentize -O style=monokai -f console256 -g'
-
 # find
 function find_i {
   find . -iname "*$@*" | grep -i "$@"
 }
 alias ff="find_i"
 alias f="find_i"
-
-# Fixing  'v', fasd alias
-alias v='fasd -e vim'
 
 # Untar
 alias untar="tar -xvf"
@@ -78,7 +72,6 @@ alias doc="cd ~/Documents"
 alias des="cd ~/Downloads"
 alias D="cd ~/Desktop/"
 alias W="cd ~/wrk"
-alias kk="cd /tmp"
 
 # 'which' shortcut (it will copy the result to clipboard)
 # Example: 'w grep'
@@ -110,9 +103,6 @@ alias pw="pwd | tr -d '\n' | xsel -ib; pwd"
 # Ping
 alias ping="ping -c 3"
 
-# Meld
-alias m.="meld ."
-
 # Catkin
 alias cm="catkin_make"
 
@@ -128,67 +118,4 @@ alias ta='tmux attach'
 
 # Others
 alias free="free -m"
-alias alarma="aplay /home/spe/Documents/Sound/finish.wav"
-alias mensaje="notify-send --urgency=critical --expire-time=0 \"ATENCIÓN\" \"Terminó!\" --icon=/usr/share/icons/gnome/32x32/status/important.png"
-alias open_microphone="arecord -f cd -D hw:0,0  | aplay"
-alias temp="cat /proc/acpi/thermal_zone/THM/temperature"
-alias LOG="svn log -l 10 -r HEAD:1 | less"
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
-
-function get_build_folder_name() {
-    # default folder for compilation
-    BUILDS="$HOME/build"
-
-    # pwd
-    CURRENT_DIR=`pwd | sed 's/ /_/g'`  # replace space by '_'
-    BASE=`basename $CURRENT_DIR`
-
-    # echo $1
-    NAME=$1
-    if [ -z "$NAME" ]; then
-        NAME="build"
-    fi
-    FOLDER=$BUILDS/${BASE}_${NAME}
-}
-
-# separe "build/" directory from the actually "wrk/" folder
-function mkbuild() {
-    get_build_folder_name $1
-
-    # create folder
-    printf "Creating folder in: \n\t%s\n\n" $FOLDER
-    if [ -d "$FOLDER" ]
-    then
-        printf "Directory already exists! Solution:\n"
-        printf "\trm -r $FOLDER\n\n"
-        return 1
-    fi
-    mkdir -p $FOLDER
-
-#     # create symbolic link
-#     if [ -d "$NAME" ]
-#     then
-#         printf "Symbolic link already exists! Solution:\n"
-#         printf "\trm -r $NAME\n\n"
-#         return 1
-#     fi
-#     ln -s $FOLDER $NAME
-#     ls -l $NAME
-#     echo ""
-
-    cd $FOLDER
-    echo cmake $CURRENT_DIR -DCMAKE_INSTALL_PREFIX="$HOME/install/${BASE}_${NAME}"
-    cmake $CURRENT_DIR -DCMAKE_INSTALL_PREFIX="$HOME/install/${BASE}_${NAME}"
-}
-
-function rmbuild() {
-    get_build_folder_name $1
-    echo rm -rf $FOLDER
-    rm -rf $FOLDER
-}
-
-function cdbuild() {
-    get_build_folder_name $1
-    echo $FOLDER
-    cd $FOLDER
-}
